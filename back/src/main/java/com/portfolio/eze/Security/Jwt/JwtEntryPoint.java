ed0d2524b97dc6867d940817d42ea7233fe8c161
ecerella <ecerella@gmail.com>
2023-01-13 00:13:25 -0300

    alargo el tiempo de expiracion del token

--- back/src/main/java/com/portfolio/eze/Security/Jwt/JwtEntryPoint.java
+++ back/src/main/java/com/portfolio/eze/Security/Jwt/JwtEntryPoint.java
@@ -20,7 +20,7 @@ public class JwtEntryPoint implements AuthenticationEntryPoint{
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        logger.error("falla de metodo commence");
+        logger.error("Falló el metodo commence ");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
     
