f51ddd7d44ccd74887608a7cd8f05f532f13a2a4
ecerella <ecerella@gmail.com>
2023-04-21 14:47:53 -0300

    me cargue el repo, pruebo reconstruccion3.2

--- back/src/main/java/com/portfolio/eze/Controller/PersonaController.java
+++ back/src/main/java/com/portfolio/eze/Controller/PersonaController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/personas")
 @CrossOrigin(origins = {"https://portfolioeze.web.app","http://localhost:4200"})
